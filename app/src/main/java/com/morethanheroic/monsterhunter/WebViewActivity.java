833cae453370cba089db3bc8295e76448d49d86c
Kumaresan C <kumaresh.btech09@gmail.com>
2018-06-08 13:08:10 +0530

    [Layout] Fixed the title and layout design

--- app/src/main/java/com/morethanheroic/monsterhunter/WebViewActivity.java
+++ app/src/main/java/com/morethanheroic/monsterhunter/WebViewActivity.java
@@ -33,8 +33,8 @@ public class WebViewActivity extends Activity {
 
     private void setupContentContainer() {
         setContentView(R.layout.activity_web_view);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
     }
 
     private void setupWebView() {
